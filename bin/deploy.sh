0af50f0c13810bd00020a916e3fa0af1f0bb6bbd
Morten Linderud <morten@linderud.pw>
2020-12-23 18:46:58 +0100

    Added cname stuff
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,5 +4,6 @@ GIT_WORKINGDIR=$(mktemp -d .tmp.XXXX)
 trap 'rm -rf "$GIT_WORKINGDIR"' EXIT
 git worktree add "$GIT_WORKINGDIR" gh-pages
 mdbook build -d "$GIT_WORKINGDIR/docs"
+printf "secureboot.dev" > "$GIT_WORKINGDIR/docs/CNAME"
 (cd "$GIT_WORKINGDIR"; git add .; git commit -am "Updated webpage"; git push origin gh-pages) 
 git worktree remove "$GIT_WORKINGDIR"
